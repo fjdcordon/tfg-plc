f46c949c678b478507af5617125b881aeedbca51
fjdcordon <fjdcordon@hotmail.com>
2022-06-24 09:53:58 +0000

    Añadido script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo plc > /etc/hostname
+userdel -r vagrant
+cp db.sqlite3 funciones.py plc updateAngle.py /root
+cp plc.service /etc/systemd/system
+cd updateAngle.service /etc/systemd/system
+pip install apscheduler
+systemctl enable plc
+systemctl enable updateAngle
+cp eth0.network /etc/systemd/network
+shutdown
