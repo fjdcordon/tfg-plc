2072be25ac9f758154e8690e1043efcd750fef0a
fjdcordon <105855130+fjdcordon@users.noreply.github.com>
2022-06-24 13:00:36 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ userdel -r vagrant
 cp db.sqlite3 funciones.py plc updateAngle.py /root
 cp plc.service /etc/systemd/system
 cp updateAngle.service /etc/systemd/system
-chmod +x /root/plc
 ln -s /root/plc /usr/local/bin/plc
 pip install apscheduler
 systemctl enable plc
